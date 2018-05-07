e86db868d66728d78ee8aa3046568e16325848c3
trinhngockhang <trinhngockhang1503@gmail.com>
2018-05-07 23:01:21 +0700

    up image

--- shared/config/api-config.js
+++ shared/config/api-config.js
@@ -1,3 +1,5 @@
 exports.FIND_PRODUCTBYNAME = "/find-product-by-name";
 exports.FIND_PRODUCTBYCATEGORY = "/find-product-by-category";
 exports.CREATE_PRODUCT = "/create-product";
+exports.CREATE_CATEGORY = "/create-category";
+exports.GETCATEGORY = "/get-category"
