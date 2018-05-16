ae1509bea08be1737fbe4c066fd08b65af464d3a
anlancan <anlancan96@gmail.com>
2018-05-16 16:06:53 +0700

    template

---
+++ client/src/components/common/header/Loadfile.js
@@ -0,0 +1,24 @@
+import Head from 'next/head';
+
+export const Loadfile = () => {
+    return (
+        <Head>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+            <meta property="og:locale" content="en_US" />
+            <meta property="og:type" content="article" />
+            <meta property="og:title" content="Simple Slider - Flatsome" />
+            <meta property="og:url" content="http://flatsome3.uxthemes.com/demos/shop-demos/simple-slider/" />
+            <meta property="og:site_name" content="Flatsome" />
+            <meta property="og:image" content="http://flatsome3.uxthemes.com/wp-content/uploads/2013/08/simple-slider.jpg" />
+            <meta property="og:image:width" content="435" />
+            <meta property="og:image:height" content="365" />
+            <meta name="twitter:card" content="summary" />
+            <meta name="twitter:title" content="Simple Slider - Flatsome" />
+            <meta name="twitter:image" content="http://flatsome3.uxthemes.com/wp-content/uploads/2013/08/simple-slider.jpg" />
+            <link rel="stylesheet" href="/static/css/extend-style.css" type="text/css" media="all"/>
+            <link rel="stylesheet" href="/static/css/style.css" type="text/css" media="all"/>
+            <link rel="stylesheet" href="/static/css/custom.css" type="text/css" media="all"/> 
+        </Head>
+    )
+}
---
+++ client/src/components/common/header/SearchBox.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+function mapDispatchToProps() {
+    return {
+
+    };
+}
+
+
+class SearchBox extends Component {
+    searchItem = () => {
+        let searchKeyWord = this.searchInput.value;
+        console.log(searchKeyWord);
+    }
+
+    render() {
+        return (
+            <div className="dropdown-menu dropdown-search">
+                <div className="input-group ">
+                    <input type="text" className="search-submit-form btn" placeholder="Search" ref={ref => this.searchInput = ref }/>
+                    <span className="search-submit-button">
+                        <button className="" type="button" onClick={() => this.searchItem()}><i className="fa fa-search"></i></button>
+                    </span>
+                </div> 
+            </div>
+        );
+    }
+}
+
+export default connect(
+    null, mapDispatchToProps
+)(SearchBox);
