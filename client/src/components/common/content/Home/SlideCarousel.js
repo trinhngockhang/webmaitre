9103e114df91a3af9e62b51e1f131cd0dbfc15e0
trinhngockhang <trinhngockhang1503@gmail.com>
2018-05-25 22:49:43 +0700

    up logo

--- client/src/components/common/content/Home/SlideCarousel.js
+++ client/src/components/common/content/Home/SlideCarousel.js
@@ -4,26 +4,24 @@ export const SlideCarousel = (props) => {
             <div className="carousel slide" >   
                 <div className="carousel-inner" id="akr_home"> 
                 <div id="item0" className="item active" > 
-                    <a href="" title=""> <div className="main-img-bg" style={{backgroundImage: "url('/static/images/uploads/bannertest1.jpg')"}}></div> </a> 
-                    {/* <div className="carousel-caption"> 
-                        <div className="overlay-lower">
-                            <span>Tough Chronograph with Smartphone link</span>
-                            <b>G-STEEL</b>
-                            <h2 className="ls-l-1">GST-B100 Series</h2> 
+                    <a href="" title=""> <div className="main-img-bg" style={{backgroundImage: "url('/static/images/uploads/bannertest4.jpg')"}}></div> </a> 
+                    <div className="carousel-caption"> 
+                        <div className="overlay-lower" >
+                            <h4 className="ls-l-1" >Hoàng Quý</h4> 
+                            <span>Chuyên thi công mái che</span> 
                         </div>
-                        <a href="" className="btn btn-default" tabindex="0">Learn More</a>
-                    </div>  */}
+                        <a href="/product" className="btn btn-default" tabindex="0">Tìm hiểu</a>
+                    </div> 
                 </div>
                 <div id="item1" className="item" >  
                     <a href="" title=""> <div className="main-img-bg" style={{backgroundImage: "url('/static/images/uploads/bannertest2.jpg')"}}></div> </a>  
-                    {/* <div className="carousel-caption">
-                        <h2 className="ls-l-1">REFINED</h2> 
+                    <div className="carousel-caption">
+                        <h2 className="ls-l-1">Giá cực tốt</h2> 
                         <div className="overlay-lower">
-                            <span>Featuring</span>
-                            <b>MRGG2000HT-1A</b>
+                            <b>Nhiều mẫu mã</b>
                         </div>
-                        <a href="" className="btn btn-default" tabindex="0">Learn More</a>
-                    </div>  */}
+                        <a href="/product" className="btn btn-default" tabindex="0">Tìm hiểu</a>
+                    </div> 
                 </div>
                 </div>    
             </div> 
